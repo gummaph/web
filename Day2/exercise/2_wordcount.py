3ff25081d865a778ce26ed710e168338d36ef87f
phanindra <g.phanindra@gmail.com>
2023-11-08 12:59:14 +0530

    add tracking funcationality to wordcount program

---
+++ Day2/exercise/2_wordcount.py
@@ -0,0 +1,28 @@
+import time
+
+from flask import Flask
+from flask import request
+import time
+
+app=Flask(__name__)
+def setup_tracking(called):
+    def f(*args,**kwargs):
+        request.req_id=f"Req_{time.time()*1000}"
+        request.logger=app.logger
+        return called(*args,**kwargs)
+
+    f.__name__=called.__name__
+    return f
+
+@app.route("/api/wordcount/",methods=["POST"])
+@setup_tracking
+def wordcount():
+    log_message={"tracking id":request.req_id,"operation":"word count","status":"processing"}
+    request.logger.info(str(log_message))
+
+    body=request.json
+    sentance=body['sentance']
+    words=len(sentance.split())
+    log_message["status"]="done"
+    request.logger.info(str(log_message))
+    return str(words),200
