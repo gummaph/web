8d58fb4e32ed90e28aee55a08c00f3b79200d35e
phanindra <g.phanindra@gmail.com>
2023-11-09 17:30:05 +0530

    auto format file no change in funcationality

--- Day2/exercise/2_wordcount.py
+++ Day2/exercise/2_wordcount.py
@@ -4,25 +4,28 @@ from flask import Flask
 from flask import request
 import time
 
-app=Flask(__name__)
-def setup_tracking(called):
-    def f(*args,**kwargs):
-        request.req_id=f"Req_{time.time()*1000}"
-        request.logger=app.logger
-        return called(*args,**kwargs)
+app = Flask(__name__)
 
-    f.__name__=called.__name__
+
+def setup_tracking(called):
+    def f(*args, **kwargs):
+        request.req_id = f"Req_{time.time() * 1000}"
+        request.logger = app.logger
+        return called(*args, **kwargs)
+
+    f.__name__ = called.__name__
     return f
 
-@app.route("/api/wordcount/",methods=["POST"])
+
+@app.route("/api/wordcount/", methods=["POST"])
 @setup_tracking
 def wordcount():
-    log_message={"tracking id":request.req_id,"operation":"word count","status":"processing"}
+    log_message = {"tracking id": request.req_id, "operation": "word count", "status": "processing"}
     request.logger.info(str(log_message))
 
-    body=request.json
-    sentance=body['sentance']
-    words=len(sentance.split())
-    log_message["status"]="done"
+    body = request.json
+    sentance = body['sentance']
+    words = len(sentance.split())
+    log_message["status"] = "done"
     request.logger.info(str(log_message))
-    return str(words),200
+    return str(words), 200
